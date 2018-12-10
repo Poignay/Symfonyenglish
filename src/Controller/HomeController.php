14b2144f148f66d009424afddc2afe02188545f7
Cabrol Alexis <alexiscabrololivaux@gmail.com>
2018-12-10 10:14:35 +0100

    Init first route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,14 @@
 // src/Controller/HomeController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-class HomeController
+class HomeController extends Controller
 {
     public function index()
     {
-        return $this->render('base.html.twig', null);
+        return $this->render('index.html.twig');
     }   
 }
