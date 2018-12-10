6fdeaa3a9aa486e92186032137bd330369875192
Cabrol Alexis <alexiscabrololivaux@gmail.com>
2018-12-10 15:35:17 +0100

    Init sign in controller

---
+++ src/Controller/SignInController.php
@@ -0,0 +1,15 @@
+<?php
+// src/Controller/SignInController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class SignInController extends Controller
+{
+    public function signin()
+    {
+        return $this->render('signin.html.twig');
+    }   
+}
